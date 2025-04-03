395b92af76d10cdd9d49567cec68532b05026cc9
Gaurav <gauravkatheoff@gmail.com>
2025-04-03 23:37:16 +0530

    added

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"
+/* Note :  
+1. "use strict" -> strict mode (used for showcasing that we're using strict mode means latest java script (New Standards))
+
+2. alert() -> alert statement can't be used in local machine while executing the js file in node js 
+if we want to use aler() we can embeed it into html file or use in browser console 
+alert("heyy")
+*/
+
+/* 
+Data Types in Java Script 
+1. number 
+2. bigint 
+3. string
+4. float
+5. null
+6. undefined [when we dont assign an value to an variable]
+*/
+let a=6
+let user_name = "Gaurav Kathe"
+let bool=true
+let num=100.1
+let x=null;
+let cc;
+console.table([typeof a,typeof user_name,typeof bool,typeof num,typeof x,typeof cc])
+
+console.table([a,user_name,bool,num])
--- Basics/varLet.js
+++ Basics/varLet.js
@@ -3,5 +3,4 @@ let name="Gaurav"
 acacount_no=76
 city="Bombay"
 let state;
-
 console.table([id,name,acacount_no,city,state])
