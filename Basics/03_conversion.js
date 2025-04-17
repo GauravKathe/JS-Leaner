e5528bdc5db9ab688caf1f12b0c96e7930eabb1e
Gaurav <gauravkatheoff@gmail.com>
2025-04-17 20:22:07 +0530

    revision

--- Basics/03_conversion.js
+++ Basics/03_conversion.js
@@ -1,14 +1,21 @@
 
 /* Note 
-1. In javascript we can convert ont data type to another by using the particular dataype class
+1. In javascript we can convert one data type to another by using the particular dataype class
 like 
 let a=33
 console.log(String(a))
 so the above number type will be converted to string 
 
-2.
-and if we're trying to convert 33abc string to number so in JS it gets convert like that variables datatype gets 
+2.if we're trying to convert 33abc string to number so in JS it gets convert like that variables datatype gets 
 changed but value becomes NaN - Not an Number 
+
+3.If let a = null 
+let b = Number(a);
+console.log(b); it will convert it to number and will give o/p 0 
+null => 0
+33 abc => NaN
+True  => 1
+hitesh => NaN
 
 */
 
@@ -19,6 +26,6 @@ console.log(convert + " "+ typeof(convert))
 
 let sc=1008
 let con= String(sc)
-console.table([convert,con])
+console.log(typeof  con,con)
 
 //console.log(2**3) //power like 2^3
--- Basics/test.js
+++
@@ -1,2 +0,0 @@
-let a="hey"
-console.table([a])
